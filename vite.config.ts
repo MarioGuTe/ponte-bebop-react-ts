7b42af1cb1353acbde0e7edf24795695a7f8c1e2
MarioGuTe <mariogutierreztello@gmail.com>
2023-10-25 01:24:20 -0300

    add build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3030,
+  },
+  preview: {
+    port: 4270,
+  },
+  // build options
+  build: {
+    incremental: true,
+    babel: {
+      presets: ["@babel/preset-env", "@babel/preset-react"],
+    },
+    typescript: {
+      tsconfig: "./tsconfig.json",
+    },
+    cache: true,
+    minify: true,
+    cssMinify: true,
+    mode: "production",
+    chunks: true,
+    moduleBundling: true,
+    prerenderPaths: ["/"],
+    modulePreload: true,
+    outDir: "build",
+  },
+});
