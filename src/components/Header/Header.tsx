02ad3df38af1331a1a21ac1aab1c78f3ddd999d8
MarioGuTe <mariogutierreztello@gmail.com>
2023-10-25 18:23:05 -0300

    add header and nav basic structure plus basic styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,30 @@
+/* eslint-disable react/self-closing-comp */
+import s from "./particle/style.module.css";
+
 function Header() {
-  return <div>This is the Header</div>;
+  return (
+    <header>
+      <nav>
+        <div className={s.nav_title_container}>
+          <h1 className="nav_title">
+            PONTE<span>BEBOP</span>
+          </h1>
+        </div>
+        <ul>
+          <li>Integrantes</li>
+          <li>Bio</li>
+          <li>Contacto</li>
+          <li>Repertorio</li>
+        </ul>
+        <div className={s.burger_menu}>
+          <div className="line_1 burger_child"></div>
+          <div className="line_2 burger_child"></div>
+          <div className="line_3 burger_child"></div>
+          <div className="line_3 burger_child"></div>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
