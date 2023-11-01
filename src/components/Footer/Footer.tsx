3f3f11cf09cd46b64e729d757a6d10e31cc90a08
MarioGuTe <mariogutierreztello@gmail.com>
2023-11-01 17:23:14 -0300

    add ireact-icons

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,30 @@
+import { Link } from "react-router-dom";
+import { FaInstagram, FaWhatsapp } from "react-icons/fa";
+
+import s from "./particle/style.module.css";
+
 function Footer() {
-  return <footer>This is the footer</footer>;
+  return (
+    <footer>
+      <p>© PonteBebop Santiago de Chile 2023</p>
+      <div className={s.footer_icons_container}>
+        <Link
+          to="https://www.instagram.com/pontebebop"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram />
+        </Link>
+        <Link
+          to="https://www.instagram.com/pontebebop"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaWhatsapp />
+        </Link>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
