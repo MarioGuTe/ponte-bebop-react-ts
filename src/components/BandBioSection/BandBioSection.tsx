86b8494f67c353b608650263b4a4f9da41473dc4
MarioGuTe <mariogutierreztello@gmail.com>
2023-10-31 23:32:09 -0300

    add BandBioSection

---
+++ src/components/BandBioSection/BandBioSection.tsx
@@ -0,0 +1,25 @@
+import s from "./particle/style.module.css";
+
+function BandBioSection() {
+  return (
+    <section className={s.band_bio_section}>
+      <div>
+        <h2>BIOGRAFÍA</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+          scelerisque rhoncus ligula vel vestibulum. Nulla mattis fermentum
+          tellus vitae pretium. Aenean vitae tincidunt eros, eget tempus sem.
+          Donec eu vulputate massa. Donec euismod malesuada est nec pretium.
+          Fusce eu mi nec erat consequat volutpat. Nullam eget odio quis urna
+          vulputate tristique. Duis volutpat dapibus justo, sed tempor dui
+          tempor vel. Proin vel viverra arcu. Vestibulum vel turpis at ex
+          tincidunt mattis. Aliquam eu pharetra nisl. Pellentesque odio lectus,
+          tempor sodales tortor sit amet, vulputate maximus nibh. Donec vitae
+          pretium nibh. In mollis venenatis arcu, quis porttitor mi euismod et.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default BandBioSection;
