c1f9a01eb93f61ef1a4a8ddcdf7d864ecb5e0b6e
MarioGuTe <mariogutierreztello@gmail.com>
2023-11-04 00:17:30 -0300

    add NotFoundPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
-import MemberBio from "./pages/MemberBio/MemberBio";
+import BioPage from "./pages/BioPage/BioPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -9,8 +11,10 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/bio" element={<MemberBio />} />
+        <Route path="/bio" element={<BioPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
+      <Footer />
     </>
   );
 }
---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,7 @@
+import s from "./particle/style.module.css";
+
+function NotFoundPage() {
+  return <section className={s.not_found_section}>Not Found Page</section>;
+}
+
+export default NotFoundPage;
