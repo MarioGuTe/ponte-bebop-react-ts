34ba147363546c20c86a88285b3f9c694be1e62d
MarioGuTe <mariogutierreztello@gmail.com>
2023-11-06 01:06:03 -0300

    add props

--- src/components/BioCardLinks/BioCardLinks.tsx
+++ src/components/BioCardLinks/BioCardLinks.tsx
@@ -3,30 +3,24 @@ import { FaInstagram, FaWhatsapp } from "react-icons/fa";
 import { AiOutlineMail } from "react-icons/ai";
 import s from "./particle/style.module.css";
 
-function BioCardLinks() {
+type Props = {
+  instagram: string;
+  mail: string;
+  whatsapp: string;
+};
+
+function BioCardLinks({ instagram, mail, whatsapp }: Props) {
   return (
     <div className={s.links_section}>
       <h3>PARA CLASES O CONSULTAS</h3>
       <div className={s.icons_container}>
-        <Link
-          to="https://www.instagram.com/pontebebop"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Link to={instagram} target="_blank" rel="noopener noreferrer">
           <FaInstagram />
         </Link>
-        <Link
-          to="https://www.instagram.com/pontebebop"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Link to={mail} target="_blank" rel="noopener noreferrer">
           <AiOutlineMail />
         </Link>
-        <Link
-          to="https://www.instagram.com/pontebebop"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Link to={whatsapp} target="_blank" rel="noopener noreferrer">
           <FaWhatsapp />
         </Link>
       </div>
