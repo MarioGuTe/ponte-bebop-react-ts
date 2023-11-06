b1c4f6039ee3152be3890d3a2b9c31f1d73d0976
MarioGuTe <mariogutierreztello@gmail.com>
2023-11-06 01:05:23 -0300

    add useMemberContext and props consumption

--- src/components/BioCard/BioCard.tsx
+++ src/components/BioCard/BioCard.tsx
@@ -1,24 +1,30 @@
-import data from "../../data/members.json";
+import { useMemberContext } from "../../contexts/MemberContext";
 import BioCardLinks from "../BioCardLinks/BioCardLinks";
 import s from "./particle/style.module.css";
 
 function BioCard() {
+  const { memberCard } = useMemberContext();
+
   return (
     <div className={s.card_container}>
       <div className={`${s.card} ${s.hidden}`}>
         <section className={s.profile_sidebar}>
           <picture>
             <img
-              src={data.members[0].small_image}
+              src={memberCard.small_image}
               className={s.profile_image}
               alt=""
             />
           </picture>
         </section>
         <section className={s.profile_main}>
-          <h2 className={s.profile_name}>{data.members[0].name}</h2>
-          <p className={s.profile_body}>{data.members[0].description}</p>
-          <BioCardLinks />
+          <h2 className={s.profile_name}>{memberCard.name}</h2>
+          <p className={s.profile_body}>{memberCard.description}</p>
+          <BioCardLinks
+            instagram={memberCard.instagram}
+            mail={memberCard.mail}
+            whatsapp={memberCard.whatsapp}
+          />
         </section>
       </div>
     </div>
