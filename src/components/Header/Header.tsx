13dca36db48cce44b2ac71f38e72e235f6909e64
MarioGuTe <mariogutierreztello@gmail.com>
2023-11-20 03:05:55 -0300

    add handleKeyDown function

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,107 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import HamburgerIcon from "../HamburgerIcon/HamburgerIcon";
 import s from "./particle/style.module.css";
 
-function Header() {
+type Props = {
+  refs: {
+    membersSectionRef: React.MutableRefObject<HTMLElement | null>;
+    bioSectionRef: React.MutableRefObject<HTMLElement | null>;
+    contactSectionRef: React.MutableRefObject<HTMLElement | null>;
+  };
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function Header({ refs }: Props, _ref: unknown) {
   const [showNavbar, setShowNavbar] = useState<boolean>(false);
 
   const handleShowNavbar = (toggled: boolean) => {
     return toggled ? setShowNavbar(true) : setShowNavbar(false);
   };
 
+  const goToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const scrollToSection = (
+    currentRef: React.MutableRefObject<HTMLElement | null>
+  ) => {
+    window.scrollTo({
+      top: currentRef.current?.offsetTop,
+      behavior: "smooth",
+    });
+  };
+
+  const handleKeyDown = (
+    e: React.KeyboardEvent<HTMLDivElement>,
+    scrollFunction: () => void
+  ) => {
+    if (e.key === "Enter" || e.key === "Space") {
+      scrollFunction();
+    }
+  };
+
   return (
     <header>
       <nav>
-        <div className={s.nav_title_container}>
+        <div
+          role="button"
+          tabIndex={0}
+          className={s.nav_title_container}
+          onClick={goToTop}
+          onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) =>
+            handleKeyDown(e, goToTop)
+          }
+        >
           <h1 className="nav_title">
             PONTE<span>BEBOP</span>
           </h1>
         </div>
         <ul className={`${showNavbar && s.active}`}>
-          <li>Integrantes</li>
-          <li>Bio</li>
-          <li>Contacto</li>
+          <li>
+            <div
+              role="button"
+              tabIndex={0}
+              onClick={() => {
+                scrollToSection(refs.membersSectionRef);
+              }}
+              onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) =>
+                handleKeyDown(e, () => scrollToSection(refs.membersSectionRef))
+              }
+            >
+              Integrantes
+            </div>
+          </li>
+          <li>
+            <div
+              role="button"
+              tabIndex={0}
+              onClick={() => {
+                scrollToSection(refs.bioSectionRef);
+              }}
+              onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) =>
+                handleKeyDown(e, () => scrollToSection(refs.bioSectionRef))
+              }
+            >
+              Bio
+            </div>
+          </li>
+          <li>
+            <div
+              role="button"
+              tabIndex={0}
+              onClick={() => {
+                scrollToSection(refs.contactSectionRef);
+              }}
+              onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) =>
+                handleKeyDown(e, () => scrollToSection(refs.contactSectionRef))
+              }
+            >
+              Contacto
+            </div>
+          </li>
           <li>Repertorio</li>
         </ul>
         <HamburgerIcon onItemClick={handleShowNavbar} />
@@ -29,4 +110,4 @@ function Header() {
   );
 }
 
-export default Header;
+export default React.forwardRef(Header);
