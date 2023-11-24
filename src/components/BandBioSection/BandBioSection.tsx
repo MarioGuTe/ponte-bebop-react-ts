7480c78e2aef1cec770e97626992620573a2f46d
MarioGuTe <mariogutierreztello@gmail.com>
2023-11-24 00:23:46 -0300

    add responsive behaviour

--- src/components/BandBioSection/BandBioSection.tsx
+++ src/components/BandBioSection/BandBioSection.tsx
@@ -10,23 +10,35 @@ type Props = {
 function BandBioSection({ refProp }: Props, _ref: unknown) {
   return (
     <section className={s.band_bio_section} ref={refProp}>
-      <div className={s.bio_text_container}>
-        <h2>BIOGRAFÍA</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-          scelerisque rhoncus ligula vel vestibulum. Nulla mattis fermentum
-          tellus vitae pretium. Aenean vitae tincidunt eros, eget tempus sem.
-          Donec eu vulputate massa. Donec euismod malesuada est nec pretium.
-          Fusce eu mi nec erat consequat volutpat. Nullam eget odio quis urna
-          vulputate tristique. Duis volutpat dapibus justo, sed tempor dui
-          tempor vel. Proin vel viverra arcu. Vestibulum vel turpis at ex
-          tincidunt mattis. Aliquam eu pharetra nisl. Pellentesque odio lectus,
-          tempor sodales tortor sit amet, vulputate maximus nibh. Donec vitae
-          pretium nibh. In mollis venenatis arcu, quis porttitor mi euismod et.
+      <h2>BIOGRAFÍA</h2>
+      <div className={s.bio_main_container}>
+        <p className={s.text_1}>
+          PonteBebop es un cuarteto de jazz formado en 2022 en Santiago de
+          Chile. Tiene la particularidad explorar y rescatar el característico
+          sonido &quot;Bebop&quot; el cual gozó de gran popularidad en
+          Norteamérica en las décadas de los 40&apos;s y 50&apos;s de la mano de
+          los grandes referentes del estilo como Charlie Parker, Bud Powell y
+          Dizzy Gillespie por mencionar algunos.
         </p>
-      </div>
-      <div className={s.bio_img_container}>
-        <img src={bioImage} alt="ponte-bebop-performing" loading="lazy" />
+        <p className={s.text_2}>
+          PonteBebop está compuesta por Benny Gómez en Batería, Nicolás Perales
+          en Contrabajo, Mario Gutiérrez en Guitarra Eléctrica quiénes se
+          conocieron como estudiantes de I.P Projazz institución de gran
+          trayectoria en la formación de músicos, el cuarteto se completa con la
+          incorporación de Emilio Hartard en Piano el cuál les entrega un nuevo
+          aire.
+        </p>
+        <p className={s.text_3}>
+          La banda ha demostrado su energía en vivo en diferentes escenarios,
+          Jam Sessions e instancias de la noche capitalina destacando por su
+          natural capacidad de improvisación, pulida técnica y un variado
+          repertorio de clásicos del &quot;Great American SongBook&quot; sin
+          perder la frescura y soltura las cuáles son características esenciales
+          del Jazz y especialmente del Bebop
+        </p>
+        <div className={s.bio_img_container}>
+          <img src={bioImage} alt="ponte-bebop-performing" loading="lazy" />
+        </div>
       </div>
     </section>
   );
