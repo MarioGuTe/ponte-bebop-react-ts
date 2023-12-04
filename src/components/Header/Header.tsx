1eaa216ed4fc3a2b168e7c06acb34e314a6ac6f8
MarioGuTe <mariogutierreztello@gmail.com>
2023-12-04 16:35:07 -0300

    add dynamic nav depending on route pathname with uselocation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useLocation, NavLink } from "react-router-dom";
 import HamburgerIcon from "../HamburgerIcon/HamburgerIcon";
 import s from "./particle/style.module.css";
 
@@ -13,6 +14,7 @@ type Props = {
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function Header({ refs }: Props, _ref: unknown) {
   const [showNavbar, setShowNavbar] = useState<boolean>(false);
+  const location = useLocation();
 
   const handleShowNavbar = (toggled: boolean) => {
     return toggled ? setShowNavbar(true) : setShowNavbar(false);
@@ -46,65 +48,88 @@ function Header({ refs }: Props, _ref: unknown) {
   return (
     <header>
       <nav>
-        <div
-          role="button"
-          tabIndex={0}
-          className={s.nav_title_container}
-          onClick={goToTop}
-          onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) =>
-            handleKeyDown(e, goToTop)
-          }
-        >
-          <h1 className="nav_title">
-            PONTE<span>BEBOP</span>
-          </h1>
-        </div>
-        <ul className={`${showNavbar && s.active}`}>
-          <li>
+        {location.pathname === "/" ? (
+          <>
             <div
               role="button"
               tabIndex={0}
-              onClick={() => {
-                scrollToSection(refs.membersSectionRef);
-              }}
+              className={s.nav_title_container}
+              onClick={goToTop}
               onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) =>
-                handleKeyDown(e, () => scrollToSection(refs.membersSectionRef))
+                handleKeyDown(e, goToTop)
               }
             >
-              Integrantes
+              <h1 className="nav_title">
+                PONTE<span>BEBOP</span>
+              </h1>
             </div>
-          </li>
-          <li>
-            <div
-              role="button"
-              tabIndex={0}
-              onClick={() => {
-                scrollToSection(refs.bioSectionRef);
-              }}
-              onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) =>
-                handleKeyDown(e, () => scrollToSection(refs.bioSectionRef))
-              }
-            >
-              Bio
+            <ul className={`${showNavbar && s.active}`}>
+              <li>
+                <div
+                  role="button"
+                  tabIndex={0}
+                  onClick={() => {
+                    scrollToSection(refs.membersSectionRef);
+                  }}
+                  onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) =>
+                    handleKeyDown(e, () =>
+                      scrollToSection(refs.membersSectionRef)
+                    )
+                  }
+                >
+                  Integrantes
+                </div>
+              </li>
+              <li>
+                <div
+                  role="button"
+                  tabIndex={0}
+                  onClick={() => {
+                    scrollToSection(refs.bioSectionRef);
+                  }}
+                  onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) =>
+                    handleKeyDown(e, () => scrollToSection(refs.bioSectionRef))
+                  }
+                >
+                  Bio
+                </div>
+              </li>
+              <li>
+                <div
+                  role="button"
+                  tabIndex={0}
+                  onClick={() => {
+                    scrollToSection(refs.contactSectionRef);
+                  }}
+                  onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) =>
+                    handleKeyDown(e, () =>
+                      scrollToSection(refs.contactSectionRef)
+                    )
+                  }
+                >
+                  Contacto
+                </div>
+              </li>
+              <li>Repertorio</li>
+            </ul>
+            <HamburgerIcon onItemClick={handleShowNavbar} />
+          </>
+        ) : (
+          <>
+            <div className={s.nav_title_container}>
+              <NavLink to="/">
+                <h1 className="nav_title">
+                  PONTE<span>BEBOP</span>
+                </h1>
+              </NavLink>
             </div>
-          </li>
-          <li>
-            <div
-              role="button"
-              tabIndex={0}
-              onClick={() => {
-                scrollToSection(refs.contactSectionRef);
-              }}
-              onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) =>
-                handleKeyDown(e, () => scrollToSection(refs.contactSectionRef))
-              }
-            >
-              Contacto
-            </div>
-          </li>
-          <li>Repertorio</li>
-        </ul>
-        <HamburgerIcon onItemClick={handleShowNavbar} />
+            <NavLink to="/">
+              <div>
+                <p className={s.go_back}>Regresar</p>
+              </div>
+            </NavLink>
+          </>
+        )}
       </nav>
     </header>
   );
